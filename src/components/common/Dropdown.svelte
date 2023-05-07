dd12fe07bbdaee6d146046da3a4cd890095f1956
dale-vand <dale.vand@kakaoenterprise.com>
2023-05-07 10:18:35 +0900

    :art: 드롭다운 구현

---
+++ src/components/common/Dropdown.svelte
@@ -0,0 +1,13 @@
+<select class="m(10/20)" name="category" id="category">
+  <option value="1">다이어트</option>
+</select>
+
+<style>
+  select {
+    border: none;
+    border-bottom: 1px solid #000;
+  }
+  select:focus {
+    outline: none;
+  }
+</style>
--- src/routes/my/+page.svelte
+++ src/routes/my/+page.svelte
@@ -1,5 +1,8 @@
 <script>
   import Header from "../../components/Header.svelte";
+  import Dropdown from "../../components/common/Dropdown.svelte";
 </script>
 
 <Header />
+
+<Dropdown />
